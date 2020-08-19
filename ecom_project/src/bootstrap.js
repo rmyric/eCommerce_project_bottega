a141c244e4a1a831c2fd8d7941215dbf5c671824
rmyric <rmyric1@gmail.com>
2020-08-19 07:45:52 -0400

    second commit

---
+++ ecom_project/src/bootstrap.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import { Router, Switch, Route } from 'react-router-dom';
+import reducers from './reducers';
+
+const createStoreWithMiddleware = applyMiddleware()(compose((window.devToolsExtension ? window.devToolsExtension() : f => f)(createStore)));
+
+import './style/main.scss';
+
+import history from './history';
+
+import Layout from './components/layout';
+// import Signin from './components/auth/signin';
+// import Signup from './components/auth/signup';
+// import Account from './components/account/account';
+// import Shop from './components/shop/shop';
+// import Review from './components/order/review';
+// import Shipping from './components/information/shipping';
+// import Payment from './components/information/payment';
+
+function main() {
+  ReactDOM.render(
+    <Provider store={createStoreWithMiddleware(reducers)}>
+      <Router history={history}>
+        <Layout>
+          <Switch>
+         
+          </Switch>
+        </Layout>
+      </Router>
+    </Provider>
+    , document.querySelector('.app-wrapper'));
+}
+
+document.addEventListener('DOMContentLoaded', main);
