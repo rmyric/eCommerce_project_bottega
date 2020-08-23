04adba0e8d4782184cd43241b4faaccad1304ad9
rmyric <rmyric1@gmail.com>
2020-08-23 11:18:32 -0400

    react-redux-form

--- ecom_project/src/bootstrap.js
+++ ecom_project/src/bootstrap.js
@@ -21,8 +21,9 @@ function main() {
       <Router history={history}>
         <Layout>
           <Switch>
-            <Route path='/' ></Route>
-         
+            <Route path='/' exact component={Signin}></Route>
+            <Route path='/signin' exact component={Signin}></Route>
+            <Route path='/signup' exact component={Signup}></Route>
           </Switch>
         </Layout>
       </Router>
