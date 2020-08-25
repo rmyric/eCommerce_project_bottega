fbffb29306c7945d9379459aef4da0d27e2a55c8
rmyric <rmyric1@gmail.com>
2020-08-24 21:59:55 -0400

    built form input email and password

--- ecom_project/src/components/auth/signinForm.js
+++ ecom_project/src/components/auth/signinForm.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
 
-import { reduxForm, Field } from 'redux-form';
+import { reduxForm, Field, Form } from 'redux-form';
+
+import { FormInput } from '../formFields';
 
 class SignInForm extends Component {
     render() {
         const { className } = this.props;
         return (
             <form className={`${className} sign-in-form`}>
-                sign inn
+                <Field className='sign-in-form__email'
+                    type='email'
+                    title='email'
+                    placeholder='email'
+                    name='email' 
+                    component={FormInput}/>
+                    <Field className='sign-in-form__password'
+                    type='password'
+                    title='password'
+                    placeholder='password'
+                    name='password' 
+                    component={FormInput}/>
             </form>
         )
     }
