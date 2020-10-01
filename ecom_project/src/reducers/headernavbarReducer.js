a7fc9cfdef52328bf29e6aa258e4e29db444814a
rmyric <rmyric1@gmail.com>
2020-10-01 08:02:52 -0400

    finished navbar feature

--- ecom_project/src/reducers/headernavbarReducer.js
+++ ecom_project/src/reducers/headernavbarReducer.js
@@ -1,32 +1,14 @@
 import {
     SET_HEADER_LINKS,
-    SET_NAVBAR_LINKS
+    SET_NAVBAR_LINKS,
+    CHANGE_NAVBAR_ACTIVE
 } from '../actions/types';
 
 
 const INITIAL_STATE = {
-    headerLinks: [
-        {
-            title: 'yooo'
-        },
-        {
-            title: 'YOOO'
-        }
-    ],
-    navbarLinks: [
-        {
-            title: 'Account',
-            active: true
-        },
-        {
-            title: 'Purchases',
-            active: false
-        }
-    ]
+    headerLinks: [],
+    navbarLinks: []
 }
-        
-
-
 export default function(state = INITIAL_STATE, action) {
     switch (action.type) {
         case SET_HEADER_LINKS:
@@ -38,6 +20,18 @@ export default function(state = INITIAL_STATE, action) {
             return {
                 ...state,
                 navbarLinks: action.payload
+            }
+        case CHANGE_NAVBAR_ACTIVE: 
+            const navbarLinks = state.navbarLinks.map(link => {
+                link.active = false;
+                if(link._id == action.payload) {
+                    link.active = true;
+                }
+                return link;
+            })
+            return {
+                ...state,
+                navbarLinks
             }
         default: return state;
     }
