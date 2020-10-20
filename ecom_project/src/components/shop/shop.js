50a47fb239800dbdd34ca818ec105d5dc5dda4c6
rmyric <rmyric1@gmail.com>
2020-10-20 18:10:47 -0400

    cart aniamations and cart button in shop component

--- ecom_project/src/components/shop/shop.js
+++ ecom_project/src/components/shop/shop.js
@@ -5,6 +5,9 @@ import * as actions from '../../actions';
 import ShopSearchBar from './shopSearchBar';
 import ShopProduct from './shopProduct';
 import ShopCart from './shopCart';
+import CartButton from './cartButton';
+
+
 
 class Shop extends Component {
 
@@ -42,6 +45,16 @@ class Shop extends Component {
         this.props.filterProductsWithQuery(fields)
     }
 
+    handleAddToCart = () => {
+        if(document.getElementById('shop-cart').classList.contains('cart-hidden')) {
+            document.getElementById('shop-cart').classList.remove('cart-hidden');
+        } else {
+            document.getElementById('shop-cart').classList.add('cart-hidden');
+        }
+    }
+
+
+
     render() {
         // return <ShopCart className='shop__cart'/>
         return (
@@ -60,7 +73,7 @@ class Shop extends Component {
                     this.state.showCart ? <ShopCart className='shop__cart'/> : ''
                 }
                 
-                {/* shop cart button */}
+                <CartButton onClick={this.handleAddToCart} className='shop__cart-button' icon='fas fa-cart-plus'/>
             </div>
         )
     }
