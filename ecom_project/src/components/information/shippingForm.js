1cc5cbd8d20ae3a4c69a645b6a0fdf68ad679300
rmyric <rmyric1@gmail.com>
2020-11-12 21:14:59 -0500

    finished shipping form component and sub components

--- ecom_project/src/components/information/shippingForm.js
+++ ecom_project/src/components/information/shippingForm.js
@@ -61,7 +61,7 @@ class ShippingForm extends Component {
                 short={true}
                 component={FormButton}/>
 
-                <OrderSummary className='shipping-form__summary'/>
+                <OrderSummary className='shipping-form__order-summary'/>
 
             </form>
         )
